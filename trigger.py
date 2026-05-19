3e66b9241e828c1b4d47d16d29480f3e63f375d2
gluecka <happy16v@gmail.com>
2026-05-19 11:51:53 +0100

    [CALIBRATION] new sensor

--- trigger.py
+++ trigger.py
@@ -18,8 +18,8 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
 
 # identified values of soil_value_ident.py
-low_wather = 21370
-high_wather = 10000
+low_wather = 21600
+high_wather = 15000
 
 # define the wathering time if the wathering system is in progress --> in scounds
 wathering_time = 1200
