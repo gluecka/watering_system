40143c25320b6292c23c851688f23755fcce4d19
Glück Arnold <162556121+gluecka@users.noreply.github.com>
2026-06-03 06:59:54 +0200

    Update data_wathering.py

--- data_wathering.py
+++ data_wathering.py
@@ -31,7 +31,7 @@ ads = ADS.ADS1115(i2c)
 # write the value in the variable--> low_wather if the sensor is absolutely dry
 # write the value in the variable--> high_wather if the sensor is in wather
 low_wather = 23000
-high_wather = 11000
+high_wather = 10000
 
 # define fuction to calculate the percent of the wather in soil
 def soil(input_measured_value):
